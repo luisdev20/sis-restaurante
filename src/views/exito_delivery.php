47741c7913c745a28602e133251cc846728483ba
luisdev20 <lescobedo.im@gmail.com>
2026-02-04 18:18:10 -0500

    feat: crear vista exito_delivery

---
+++ src/views/exito_delivery.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pedido Exitoso</title>
+    <link rel="stylesheet" href="assets/styles/reserva.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Restaurante Brasa Bros</h1>
+    </header>
+
+    <main class="fondo-exito">
+        <div class="mensaje-exito">
+            <h2>¡Pedido registrado con éxito! 🍽️</h2>
+            <p>
+                Gracias por elegirnos.<br>
+                Su pedido está siendo preparado y en breve
+                un motorizado se acercará a su domicilio.
+            </p>
+            <a href="index.php" class="btn-volver">Volver al inicio</a>
+        </div>
+    </main>
+
+    <footer>
+        © 2026 Restaurante Brasa Bros
+    </footer>
+
+</body>
+
+</html>
