e4f0dd45ac25a3782c344eb16a8ae2f865c3570f
jacksonburgac25-afk <jacksonburgac25@gmail.com>
2026-02-04 11:53:01 -0500

    Add files via upload

--- Avance 3_htdocs/conexion.php
+++ Avance 3_htdocs/conexion.php
@@ -2,7 +2,7 @@
 
 $server = "localhost";
 $username = "root";
-$password = "superadministrador";
+$password = "";
 $database = "restaurante_brasabros_db";
 $conexion = mysqli_connect($server, $username, $password, $database);
 if ($conexion -> connect_error) {
